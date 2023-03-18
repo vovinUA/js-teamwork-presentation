bd07e7fa1fb53b404b9ebaca537495f4d619e93f
vovinUA <vovin_tool@yahoo.com>
2023-03-18 19:20:31 +0200

    Gifs to webp convs and fixes

--- js/slide-show.js
+++ js/slide-show.js
@@ -11,3 +11,14 @@ phone.addEventListener('mouseleave', () => {
     el.style.filter = 'none';
   });
 });
+
+// Removing scrollbars from iframe but still able to scroll with mouse wheel
+
+let iframe = document.querySelector('iframe');
+iframe.contentWindow.addEventListener('wheel', function(e) {
+  if (iframe.contentWindow.scrollY === 0 && e.deltaY < 0) {
+    e.preventDefault();
+  } else if (iframe.contentWindow.innerHeight + iframe.contentWindow.scrollY >= iframe.contentWindow.document.body.offsetHeight && e.deltaY > 0) {
+    e.preventDefault();
+  }
+}, { passive: false });
